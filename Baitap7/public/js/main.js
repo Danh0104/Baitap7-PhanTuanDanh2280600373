c7ff42ad9e91ade12c94216238b2ac12253459b1
Danh0104 <phantuandanh1009@gmail.com>
2026-03-23 10:53:55 +0700

    Delete Baitap7 directory

--- Baitap7/public/js/main.js
+++
@@ -1,7 +0,0 @@
-// Hiển thị thời gian deploy
-const timeEl = document.getElementById('deploy-time');
-const now = new Date();
-timeEl.textContent = 'Trang được tải lúc: ' + now.toLocaleString('vi-VN');
-
-console.log('CI/CD Pipeline Demo loaded successfully!');
-console.log('GitLab Pages deployment working!');
